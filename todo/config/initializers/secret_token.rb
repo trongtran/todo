80a4d8616633e49a7a5717fbc9797ce3713cf398
Trong Tran <trongtran@asnet.com.vn>
2012-05-29 11:31:38 +0700

    Create new standard structor application

---
+++ todo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todo::Application.config.secret_token = '3ead4178330f264af388b167421af584166794dfd16db2cb979787635fb307518f24da5c44e99e12406e9338e6aea3800e5b90596ee602d3b74046374499acd5'
